823bf537aa6f39d1bf1c4e021edfc09d30f42553
Amber York <adyork@whoi.edu>
2016-07-12 14:43:36 -0400

    * Changed scripts so that it uses a dictionary for toplevel data (so I can sort then write). * Added year * Added castno from filename which I know is the castno after receiving log files from PI * skipped original file inclusion if it contained 'up' in name * created v1 folder and put the old daly_toplevel.dat in it from 7 Jul
    I still did by hand the following:
    * changed station from nd to 1 for 19502015_KD.dat (PI confirmed)

--- daly-ETP_CTD/DalyWrapper.py
+++ daly-ETP_CTD/DalyWrapper.py
@@ -2,9 +2,8 @@ import os,re
 from sys import platform as _platform
 from  DMTools import Line,xlsx2csv
 from wsgiref import headers
+from operator import itemgetter #to sort list of dictionaries
 
-#this wrapper used for dataset Daly CTD data
-# http://www.bco-dmo.org/node/651131
 
 origDir = '../intermediate_files/orig/ctd_all'
 outDir = '../intermediate_files/csv'
@@ -20,7 +19,9 @@ print "Parsing data in "+origDir
 origFiles = os.listdir(origDir)
 needConversion = False #true if you haven't yet exported to csv
 
-toplevel_header = 'cruise_id,station,castno,lat_start,lon_start,date_start,time_start'
+toplevel_header_list = ('year','cruise_id','station','castno','lat_start','lon_start','date_start','time_start');
+toplevel_header = ','.join(toplevel_header_list)
+toplevel_data_delimiter = '\t'#what goes in between toplevel datalines, not the header
 
 #header is for what is in each filename
 header= 'scan,date_string,time,lon,lat,depth,temp,temp2,potemp,potemp2,sal,cond1,cond2,sigma_e00,sigma_e11,press,O2_ml_L,O2_mg_L,O2_sat_pcnt,O2_umol_kg,sound_vel_Chen,sound_vel_Delgrosso,sound_vel_Wilson,beam_c,trans,cpar,fluor,par,spar'
@@ -52,22 +53,32 @@ topf.write(header+',ISO_DateTime_UTC\n')#need to add datetimeparam that is added
 
 print(fileComment)        
     
-castno = 1 #had to make this up becuase station number dublicate casts
+datData =[]#list of dat dictionaries (one per file)
+
 
 for filename in origFiles:
-    if re.match('^~',filename):
+    if re.search('(^~|up)',filename,re.IGNORECASE):
         continue
 
+    dat = {}#dictionary for this file
+    dat["outfile"] =re.sub('xlsx','dat',filename)
+    dat["infile"] = filename
+    
+    #make all toplevel variables nd (in case no info like ISO_DateTime_UTC
+    #cruise_id=station=lat_start=lon_start=date=time= 'nd'  #this was when I wan't using dictionary
+    for paramName in toplevel_header_list:
+        dat[paramName] = 'nd'
+    
     print(filename)
     #print "Orig: "+filename
     infile = origDir+'/'+filename
     outfile = outDir+'/'+re.sub('xlsx','csv',filename)
-    year = ''
     if re.match("^S",filename):
-        year = '2007'
+        dat["year"] = '2007'
+        dat["castno"] = re.sub('.*\.','',re.sub('_.*','',filename)) #SJ07.072.16_KD_BIN.dat
     else:
-        year = '2008'
-    
+        dat["year"] = '2008'
+        dat["castno"] = re.sub('(\d{6}|_.*)','',filename) #19502016_KD.dat
     
     if not re.match('linux',_platform):
         if needConversion:
@@ -85,10 +96,7 @@ for filename in origFiles:
     lineCount = 1
 
     dataStartAt = '' #will be changed once orig header lineText read
-    
-    #make all toplevel variables nd (in case no info like ISO_DateTime_UTC
-    cruise_id=station=lat_start=lon_start=date=time= 'nd'
-    
+     
     for origline in csvFile:
         origline = origline.rstrip('\r\n')
          
@@ -138,11 +146,11 @@ for filename in origFiles:
         #if re.match('Knorr',lineText):
         if bool(re.search('Knorr',lineText)):
             print("Line "+lineText)
-            cruise_id = 'KN195-02'
+            dat['cruise_id'] = 'KN195-02'
             
         if bool(re.search('RVSJ',lineText)):
             print("Line "+lineText)
-            cruise_id = 'SJ07' #TODO need to find num
+            dat['cruise_id'] = 'SJ07' #TODO need to find num
                 
 
         if bool(re.search('NMEA Latitude',lineText)):
@@ -151,7 +159,7 @@ for filename in origFiles:
             if re.match('S',lat_start):
                 lat_start = '-'+lat_start
             lat_start = re.sub('(N|S|E|W|\s*)','',lat_start)  
-            lat_start = re.sub('\D*$','',lat_start) 
+            dat['lat_start'] = re.sub('\D*$','',lat_start) 
              
 
         if bool(re.search('NMEA Longitude',lineText)):
@@ -160,7 +168,7 @@ for filename in origFiles:
             if re.match('W',lat_start):
                 lon_start = '-'+lon_start
             lon_start = re.sub('(N|S|E|W|\s*)','',lon_start) 
-            lon_start = re.sub('\D*$','',lon_start) 
+            dat['lon_start'] =  re.sub('\D*$','',lon_start) 
                 
         if bool(re.search('NMEA UTC',lineText)):
             print("Line "+lineText)
@@ -168,16 +176,16 @@ for filename in origFiles:
             dateparts = date_string.split(' ')
             time = dateparts[len(dateparts)-1] #time is the last one
             date = re.sub(time,'',date_string)
-            date = re.sub('\D*$','',date) #strip off any remaining non digits at end of line
-            time = re.sub('\D*$','',time) #strip off any remaining non digits at end of line
+            dat['date_start'] =  re.sub('\D*$','',date) #strip off any remaining non digits at end of line
+            dat['time_start'] =  re.sub('\D*$','',time) #strip off any remaining non digits at end of line
+            dat['date_string'] = date_string 
 
-
-        if bool(re.search('Station',lineText,re.IGNORECASE)) and station == 'nd':  #second logic to catch first description of station
+        if bool(re.search('Station',lineText,re.IGNORECASE)) and dat['station'] == 'nd':  #second logic to catch first description of station
             print("Line "+lineText)
             #this didn't work becaues matches stuff like "** 195-02-032 Station 8 deep cast (2500 m)"
             #need to have different lineText numbers for     
             station = re.sub('.*Station: ','',lineText,re.IGNORECASE)
-            station = re.sub('Station ','',station,re.IGNORECASE) #if like shown above station wrote twice\
+            station =  re.sub('Station ','',station,re.IGNORECASE) #if like shown above station wrote twice\
             
             
                         #if it is a substation like 4a or 4b we want to preserve the letter
@@ -194,6 +202,7 @@ for filename in origFiles:
                 station = 'nd'
             
             station = station.lower()
+            dat['station'] = station
                            
         lineCount += 1
     
@@ -201,11 +210,38 @@ for filename in origFiles:
     finalFile.close()
     
 
-    datLine = cruise_id+'\t'+station+'\t'+str(castno)+'\t'+lat_start+'\t'+lon_start+'\t'+date+'\t'+time+'\tcruise_data/'+re.sub('xlsx','dat',filename)+'\n'
-    print("Toplevel lineText: "+datLine)
-    topf.write(datLine)    castno += 1 #increment for each write'
-
+    #add to dat dictionary list
+    datData.append(dat)
 #end filename loop   
-topf.close()
-print "Done!"
+
+
+#--------------------now write toplevel
+
+## read back in and order by date/time
+#TODO fix sort
+    #datData = sorted(datData, key = lambda user: (user['date_start'], user['time_start']))  #this is same as below
+    #datData = sorted(datData, key=itemgetter('date_start', 'time_start'))
+for dat in datData:
+    
+    
+    #loop dictionary sorted by time and write datline
+    '''
+    datLine = year+'\t'+cruise_id+'\t'+station+'\t'+str(castno)+'\t'+lat_start+'\t'+lon_start+'\t'+date+'\t'+time+'\tcruise_data/'+re.sub('xlsx','dat',filename)+'\n'
+        print("Toplevel lineText: "+datLine)
+        topf.write(datLine)
         
+        to get decimal places
+        
+        >>> d.as_tuple().exponent
+        -4
+          '''  
+    for param in toplevel_header_list:
+        print(param+" "+dat[param]+toplevel_data_delimiter)
+
+        topf.write(dat[param]+toplevel_data_delimiter)
+        if param == toplevel_header_list[-1]: #add filename and line return  if last
+            topf.write('cruise_data/'+dat['outfile']+'\n')
+        
+topf.close()
+
+print "Done!"
